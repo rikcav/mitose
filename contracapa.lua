d811fe41b1060fadfb76de0f91c19781da00485a
rikcav <henriquearau000@gmail.com>
2024-10-21 19:45:50 -0300

    feat: added initial outlines for ebook

---
+++ contracapa.lua
@@ -0,0 +1,30 @@
+local composer = require("composer")
+local scene = composer.newScene()
+
+function scene:create(event)
+    local sceneGroup = self.view
+
+    local background = display.newImageRect(sceneGroup, "imagens/Contracapa.png", display.contentWidth, display.contentHeight)
+    background.x = display.contentCenterX
+    background.y = display.contentCenterY
+
+    local restartButton = display.newText(sceneGroup, "", 685, 930, native.systemFont, 30)
+    restartButton:setFillColor(0, 0, 1)
+
+    local prevButton = display.newText(sceneGroup, "", 48, 930, native.systemFont, 30)
+    prevButton:setFillColor(0, 0, 1)
+
+    local function gotoCapa()
+        composer.gotoScene("capa")
+    end
+
+    local function gotoPrevPage()
+        composer.gotoScene("pagina6")
+    end
+
+    restartButton:addEventListener("tap", gotoCapa)
+    prevButton:addEventListener("tap", gotoPrevPage)
+end
+
+scene:addEventListener("create", scene)
+return scene
---
+++ pagina2.lua
@@ -0,0 +1,31 @@
+local composer = require("composer")
+local scene = composer.newScene()
+
+function scene:create(event)
+    local sceneGroup = self.view
+
+    local background = display.newRect(sceneGroup, display.contentCenterX, display.contentCenterY, display.contentWidth, display.contentHeight)
+    background:setFillColor(0.9, 0.9, 0.9) -- Cinza claro
+
+    local pageText = display.newText(sceneGroup, "Página 2", display.contentCenterX, display.contentCenterY, native.systemFont, 40)
+
+    local nextButton = display.newText(sceneGroup, "Próxima página", 685, 930, native.systemFont, 30)
+    nextButton:setFillColor(0, 0, 1)
+
+    local prevButton = display.newText(sceneGroup, "Página anterior", 48, 930, native.systemFont, 30)
+    prevButton:setFillColor(0, 0, 1)
+
+    local function gotoNextPage()
+        composer.gotoScene("pagina3")
+    end
+
+    local function gotoPrevPage()
+        composer.gotoScene("capa")
+    end
+
+    nextButton:addEventListener("tap", gotoNextPage)
+    prevButton:addEventListener("tap", gotoPrevPage)
+end
+
+scene:addEventListener("create", scene)
+return scene
