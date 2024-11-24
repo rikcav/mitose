57e3bd5e5c6f66f9fc02ddb51a68471418a474ea
rikcav <henriquearau000@gmail.com>
2024-11-24 13:31:44 -0300

    feat: added back button and text

--- pags/contracapa.lua
+++ pags/contracapa.lua
@@ -9,12 +9,46 @@ function scene:create(event)
     background.x = display.contentCenterX
     background.y = display.contentCenterY
 
-    local restartButton = display.newText(sceneGroup, "INÍCIO", 685, 990, native.systemFont, 30)
-    restartButton:setFillColor(100, 100, 100, 1)
+    local homeText = display.newText(sceneGroup, "INÍCIO", 688, 990, native.systemFont, 30)
+    homeText:setFillColor(100, 100, 100, 1)
 
-    restartButton:addEventListener("tap", function()
+    homeText:addEventListener("tap", function()
         composer.gotoScene("pags.capa", {
             effect = "crossFade",
+            time = 500
+        })
+    end)
+
+    local homeButton = display.newImageRect(sceneGroup, "imagens/up_arrow.png", 60, 60)
+    homeButton.x = 687
+    homeButton.y = 940
+    homeButton.alpha = 0.1
+
+    homeButton:addEventListener("tap", function()
+        composer.gotoScene("pags.pagina2", {
+            effect = "slideLeft",
+            time = 500
+        })
+    end)
+
+    local prevText = display.newText(sceneGroup, "ANTERIOR", 88, 990, native.systemFont, 30)
+    prevText:setFillColor(100, 100, 100, 1)
+
+    prevText:addEventListener("tap", function()
+        composer.gotoScene("pags.pagina6", {
+            effect = "slideRight",
+            time = 500
+        })
+    end)
+
+    local prevButton = display.newImageRect(sceneGroup, "imagens/left_arrow.png", 60, 60)
+    prevButton.x = 80
+    prevButton.y = 940
+    prevButton.alpha = 0.1
+
+    prevButton:addEventListener("tap", function()
+        composer.gotoScene("pags.pagina6", {
+            effect = "slideRight",
             time = 500
         })
     end)
